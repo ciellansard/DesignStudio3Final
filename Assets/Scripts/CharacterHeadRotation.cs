f057027f63a244a5efbcfb2ccf8910e6cdd589d2
ciellansard <ciellansard@gmail.com>
2026-03-10 22:07:53 -0400

    Poached egg prefab created & capsule colliders on player prefabs tweaked

--- Assets/Scripts/CharacterHeadRotation.cs
+++ Assets/Scripts/CharacterHeadRotation.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+// Make the head rotate to the same direction as the camera without requiring the head to be parented to cam.
+// This is essential because the player cameras float in front of their heads to avoid the player being able to see into their body.
+// If heads were parented to the cameras, their native pivot points would not be respected (and so heads would be flying around.)
 public class CharacterHeadRotation : MonoBehaviour
 {
     public GameObject cam;
