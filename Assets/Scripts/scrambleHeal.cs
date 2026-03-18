d8430a70cce98b4356054a0fd476b99b67fb7b52
Froz3nRaspb3rry <amelia.chetelat@gmail.com>
2026-03-18 10:59:07 -0400

    Scramble complete

--- Assets/Scripts/scrambleHeal.cs
+++ Assets/Scripts/scrambleHeal.cs
@@ -5,9 +5,16 @@ using System.Diagnostics;
 public class scrambleHeal : MonoBehaviour
 {
     public int healPoints = 2;
+    public int rechargeDuration = 3;
     bool waitTime = true;
     Coroutine rechargeHeal;
     public GameObject player;
+    int defaultHealth;
+
+    private void Awake()
+    {
+        player.GetComponent<CharacterHealth>().maxHealth = defaultHealth;
+    }
 
     public void Heal()
     {
@@ -15,15 +22,15 @@ public class scrambleHeal : MonoBehaviour
         {
             waitTime = false;
 
-            if (player.GetComponent<CharacterHealth>().currentHealth < 20) {
+            if (player.GetComponent<CharacterHealth>().currentHealth < defaultHealth) {
 
                 player.GetComponent<CharacterHealth>().currentHealth = player.GetComponent<CharacterHealth>().currentHealth + healPoints;
 
-                if (player.GetComponent<CharacterHealth>().currentHealth >= 20)
+                if (player.GetComponent<CharacterHealth>().currentHealth >= defaultHealth)
                 {
-                    player.GetComponent<CharacterHealth>().currentHealth = 20;
+                    player.GetComponent<CharacterHealth>().currentHealth = defaultHealth;
                 }
-                rechargeHeal = StartCoroutine(RechargeWait(3));
+                rechargeHeal = StartCoroutine(RechargeWait(rechargeDuration));
             }
         }
         else
@@ -31,11 +38,6 @@ public class scrambleHeal : MonoBehaviour
             UnityEngine.Debug.Log("Healing not recharged");
         }
 
-    }
-
-    private void Update()
-    {
-        UnityEngine.Debug.Log(player.GetComponent<CharacterHealth>().currentHealth);
     }
 
     private IEnumerator RechargeWait(float duration)
